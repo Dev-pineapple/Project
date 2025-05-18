096bdb39eba08c7b41e7b4ab17dcd8267c3c028b
Thiago <pineappleantunes2@gmail.com>
2025-05-18 15:21:58 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+// Quando clicar no botão correspondente do personagem da lista, o botão tem que ficar marcado e a imagem do personagem correspondente tem que aparecer junto com as informações sobre ele.
 
+const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+  botao.addEventListener("click", () => {
+    
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+    botao.classList.add("selecionado");
+    
+    
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+    personagens[indice].classList.add("selecionado");
+  });
+});
